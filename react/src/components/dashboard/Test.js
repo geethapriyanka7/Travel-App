d2e9cab00cd3abdee5d2399b18195e400a9d1336
Dhruv Karve <dhruvkarve@gmail.com>
2021-12-11 16:59:45 -0500

    CSS

--- react/src/components/dashboard/Test.js
+++ react/src/components/dashboard/Test.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import axios from 'axios'
+import Button from '@mui/material/Button';
+import "./Test.css"
 
 export default function Test() {
 
@@ -21,9 +23,14 @@ export default function Test() {
 
     return (
         <div>
-            Hello
+            <p align="center">Hello World</p>
             
-
+            <div align="center">
+                <Button className="bb">Geetha</Button>
+                <br></br>
+            <Button className ="bb" variant="outlined" color="success">Kirti</Button>
+            </div>
+            
         </div>
     )
 }
