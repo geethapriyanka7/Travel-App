895d5eb7a58447595ccd00873242508e9083702e
neilsensarma <nilavrasensarma@gmail.com>
2021-12-12 17:44:28 -0500

    Backend procedures and views

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = "Pri@12364"
+app.config['MYSQL_PASSWORD'] = "Nincompoop1@"
 app.config['MYSQL_DB'] = 'travel_reservation_service'
 
 mysql = MySQL(app)
@@ -36,7 +36,7 @@ def index():
 
 
 
-@app.route("/a", methods = ['POST'])
+@app.route("/remove_owner", methods = ['POST'])
 @cross_origin()
 def index1():
         cur = mysql.connection.cursor()
@@ -97,7 +97,7 @@ def index4():
 @cross_origin()
 def index5():
         cur = mysql.connection.cursor()
-        cmd = "select * from travel_reservation_service.view_flight;"
+        cmd = "select f.Flight_Num as ID, f.Airline_Name as Airlines, f.From_Airport, f.To_Airport,f.Departure_Time as Dept_Time, f.Arrival_Time as  Arr_Time, f.Flight_Date as Date, (f.Capacity - ifnull(sum(b.Num_Seats * IF(b.Was_Cancelled,0,1)),0)) as Available_Seats, f.Cost as Cost_Per_Seat, (ifnull(sum((f.Cost*b.Num_Seats)*IF(b.Was_Cancelled,0.2,1)),0)) as Total_Spent from flight f left join book b on (f.Flight_Num, f.Airline_Name) = (b.Flight_Num, b.Airline_Name) group by ID, Airlines;"
         cur.execute(cmd)
         row_headers = [x[0] for x in cur.description]
         rv = cur.fetchall()
@@ -153,6 +153,186 @@ def index8():
         cur.close()
         return json.dumps(json_data)
 
+@app.route("/admin_process_date")
+@cross_origin()
+def index9():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call process_date('"+data[0]['date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/remove_flight")
+@cross_origin()
+def index10():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call remove_flight('"+data[0]['flight_num']+"', '"+data[0]['airline_name']+"', '"+data[0]['current_data']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/schedule_flight")
+@cross_origin()
+def index11():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call schedule_flight('"+data[0]['flight_num']+"', '"+data[0]['airline_name']+"', '"+data[0]['from_airport']+"', '"+data[0]['departure_time']+"', '"+data[0]+['arrival_time']"', '"+data[0]['flight_date']+"', '"+data[0]['cost']+"', '"+data[0]['capacity']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()        
+        cur.close()
+        return None
+
+@app.route("/register_customer")
+@cross_origin()
+def index12():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call register_customer('"+data[0]['email']+"', '"+data[0]['first_name']+"', '"+data[0]['last_name']+"', '"+data[0]['password']+"', '"+data[0]['phone_number']+"', '"+data[0]['cc_number']+"', '"+data[0]['cvv']+"', '"+data[0]['exp_data']+"', '"+data[0]['location']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None 
+
+@app.route("/register_owner")
+@cross_origin()
+def index13():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call register_owner('"+data[0]['email']+"', '"+data[0]['first_name']+"', '"+data[0]['last_name']+"', '"+data[0]['password']+"', '"+data[0]['phone_number']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/book_flight")
+@cross_origin()
+def index14():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call book_flight('"+data[0]['email']+"', '"+data[0]['flight_nu,']+"', '"+data[0]['airline_name']+"', '"+data[0]['num_seats']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None  
+
+@app.route("/cancel_flight")
+@cross_origin()
+def index15():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call cancel_flight_booking('"+data[0]['email']+"', '"+data[0]['flight_num']+"', '"+data[0]['airline_name']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/reserve_property")
+@cross_origin()
+def index16():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call reserve_property('"+data[0]['property_name']+"', '"+data[0]['owner_email']+"', '"+data[0]['customer_email']+"', '"+data[0]['start_date']+"', '"+data[0]['end_date']+"', ''"+data[0]['num_guests']+", '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/cancel_property_reservation")
+@cross_origin()
+def index17():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call cancel_property_reservation('"+data[0]['property_name']+"', '"+data[0]['owner_email']+"', '"+data[0]['customer_email']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/customer_review_property")
+@cross_origin()
+def index18():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call customer_review_property('"+data[0]['property_name']+"', '"+data[0]['owner_email']+"', '"+data[0]['customer_email']+"', '"+data[0]['content']+"', '"+data[0]['score']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+
+@app.route("/customer_rate_owner")
+@cross_origin()
+def index19():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call customer_rates_owner('"+data[0]['customer_email']+"', '"+data[0]['owner_email']+"', '"+data[0]['score']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/view_properties_reservations")
+@cross_origin()
+def index20():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call view_individual_property_reservations('"+data[0]['property_name']+"', '"+data['owner_email']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/owner_add_property")
+@cross_origin()
+def index21():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call add_property('"+data[0]['property_name']+"', '"+data[0]['owner_email']+"', '"+data[0]['description']+"', '"+data[0]['capacity']+"', '"+data[0]['cost']+"', '"+data[0]['street']+"', '"+data[0]['city']+"', '"+data[0]['state']+"', '"+data[0]['zip']+"', '"+data[0]['nearest_airport_id']+"', '"+data[0]['dist_to_airport']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/owner_remove_property")
+@cross_origin()
+def index22():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call remove_property('"+data[0]['owner_email']+"', '"+data[0]['current_date']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
+
+@app.route("/owner_rates_customer")
+@cross_origin()
+def index23():
+        cur = mysql.connection.cursor()
+        data = request.get_json()
+        cmd = "call owner_customer('"+data[0]['owner_email']+"', '"+data[0]['customer_email']+"', '"+data[0]['score']+"', '"+data[0]['current_data']+"');"
+        cur.execute(cmd)
+        #cur.fetchall()
+        mysql.connection.commit()
+        cur.close()
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
